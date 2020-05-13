9d2de402d3bab012e5f5fab54d474a628a154059
George Muteti <admin@example.com>
2020-05-13 13:20:14 +0300

    Added Sidebar and Menu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NbThemeModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbMenuModule } from '@nebular/theme';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbSidebarModule } from '@nebular/theme';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     NbThemeModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbSidebarModule.forRoot(),
+    NbSidebarModule,
+    NbMenuModule.forRoot(),
+    NbMenuModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
